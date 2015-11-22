fadf3253051a88c154a90f59793e311bae71a43d
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2015-11-22 10:10:24 +0100

    Added new weather Java example

--- hadoop-streaming-02/README.md
+++ hadoop-streaming-02/README.md
@@ -1,8 +1,23 @@
-hadoop  jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-    -files mapper.py,reducer.py,ish-history.csv \
-    -input alice \
-    -output alice-wc \
-    -mapper mapper.py \
-    -reducer reducer.py
+## Debug Mode
 
-zcat /data/weather/*.gz | ./mapper.py | sort | ./reducer.py 
+You can perform a simple simulation using shell tools
+
+        zcat /data/weather/*.gz | ./mapper.py | sort | ./reducer.py 
+
+# Uploading data
+
+        hdfs dfs -mkdir weather
+        hdfs dfs -put ~/data/weather/2011 weather
+        ./run.sh weather/2011 weather_minmax
+
+## Retrieve Results
+
+You can retrieve the results via `hdfs dfs -germerge`
+
+        hdfs dfs -getmerge weather_minmax weather_minmax
+        cat weather_minmax
+
+Or you can also view the results on the console via
+
+        hdfs dfs -cat weather_minmax/*
+        
---
+++ hadoop-weather-01/src/main/java/de/dimajix/training/hadoop/weather/WeatherMapper.java
@@ -0,0 +1,40 @@
+package de.dimajix.training.hadoop.weather;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+class WeatherMapper extends Mapper<LongWritable,Text,Text,FloatWritable> {
+    private Text country = new Text();
+    private FloatWritable temperature = new FloatWritable();
+
+    @Override
+    public void setup(Context context) {
+    }
+
+    @Override
+    public void cleanup(Context context) {
+    }
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        String row = value.toString();
+
+        String station = row.substring(4,15);
+        Character airTemperatureQuality = row.charAt(92);
+        String airTemperature = row.substring(87,92);
+
+        // Only emit if quality is okay
+        if (airTemperatureQuality.equals("1")) {
+            // country = countries[station]
+            country.set(station);
+            temperature.set(Float.valueOf(airTemperature));
+            context.write(country, temperature);
+        }
+    }
+}
