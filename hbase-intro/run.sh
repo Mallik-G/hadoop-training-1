3905beb302bcd3c8cafe38034538fa8c276e8642
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2015-12-06 15:43:35 +0100

    New HBase example

---
+++ hbase-intro/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+APP_NAME="spark-skeleton"
+APP_MAIN="de.dimajix.training.hbase.Example"
+APP_VERSION="0.1.0"
+CDH_VERSION="cdh5.4.2"
+
+JAR_NAME="target/$APP_NAME-$APP_VERSION-$CDH_VERSION-jar-with-dependencies.jar"
+
+hadoop jar $JAR_NAME $APP_MAIN $@
