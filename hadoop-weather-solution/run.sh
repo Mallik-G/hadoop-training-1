a40a4e794072257108f7b59a49729e6a5cada8f5
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2015-12-28 14:24:58 +0100

    Modified build for CDH 5.5.0

--- hadoop-weather-solution/run.sh
+++ hadoop-weather-solution/run.sh
@@ -3,7 +3,7 @@
 APP_NAME="hadoop-weather-02"
 APP_MAIN="de.dimajix.training.hadoop.weather.Weather"
 APP_VERSION="0.1.0"
-CDH_VERSION="cdh5.4.2"
+CDH_VERSION="cdh5.5.0"
 
 JAR_NAME="target/$APP_NAME-$APP_VERSION-$CDH_VERSION-jar-with-dependencies.jar"
 
